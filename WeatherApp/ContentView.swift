884bfe5bd4cb40f16c5c9593a75542fb2cbd83dc
Maso Mahboob <masomahboob@proton.me>
2023-11-04 19:03:45 +0430

    Build a gradient background on a ZStack.

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), 
+                           startPoint: .topLeading, 
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
         }
-        .padding()
     }
 }
 
-
+#Preview {
+    ContentView()
+}
