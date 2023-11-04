b5494af54143b4ac2ed197b833a6f947a5000dbe
Maso Mahboob <masomahboob@proton.me>
2023-11-04 19:13:40 +0430

    Add the location label.

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -14,6 +14,15 @@ struct ContentView: View {
                            startPoint: .topLeading, 
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Kabul, AF")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Spacer()
+            }
         }
     }
 }
