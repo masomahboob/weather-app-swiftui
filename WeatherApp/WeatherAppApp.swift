eece185fa5fb174917058f4c02194e38630d2917
Maso Mahboob <masomahboob@proton.me>
2023-11-04 18:23:36 +0430

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mosawer Mahboob on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
