547f9bb292d6a87ded10a77a7aa501a06e76d93d
Maso Mahboob <masomahboob@proton.me>
2023-11-04 19:37:33 +0430

    Add the SF Symbol for weather and the temprature number in a VStack of its own withing the whole ZStack + VStack.

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -15,11 +15,23 @@ struct ContentView: View {
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
+            VStack() {
                 Text("Kabul, AF")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("12°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
                 
                 Spacer()
             }
